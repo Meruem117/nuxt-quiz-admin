d1710bcc45d72220defcd5bcded56931c82371ba
Meruem117 <1572677319@qq.com>
2022-03-31 22:08:37 +0800

    change

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'Nuxt Quiz',
+    title: 'Nuxt Quiz Admin',
     htmlAttrs: {
       lang: 'en'
     },
