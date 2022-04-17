3470333618ab5360858429f74ab9f6dc4406e4fd
Meruem117 <1572677319@qq.com>
2022-04-17 21:11:45 +0800

    add plugin

--- plugins/element-plus.ts
+++ plugins/element-plus.ts
@@ -1,1 +1,6 @@
-export default {}
+import { defineNuxtPlugin } from '#app'
+import ElementPlus from 'element-plus/dist/index.full.js'
+
+export default defineNuxtPlugin(nuxtApp => {
+  nuxtApp.vueApp.use(ElementPlus)
+})
