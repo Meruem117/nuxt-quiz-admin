bcf980baa851ff7dcc195cd6dd70b157bde0ba96
Meruem117 <1572677319@qq.com>
2022-05-03 22:53:08 +0800

    update admin service

---
+++ services/admin.ts
@@ -0,0 +1,13 @@
+import type { responseItem, loginItem, passwordCheckItem } from '@/models/base'
+import { baseUrl } from '@/constant'
+
+export async function checkAdminPassword(login: loginItem): Promise<responseItem<passwordCheckItem>> {
+  const response = await fetch(baseUrl + '/user/check', {
+    method: 'POST',
+    body: JSON.stringify(login),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+  return response.json()
+}
